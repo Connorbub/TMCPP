11ff39daa813700206f2cf270116ef3c2be36c25
Connorbub <connorbubgames@gmail.com>
2022-02-10 20:11:30 -0500

    Added some more example TMs

--- TuringMachine.h
+++ TuringMachine.h
@@ -8,55 +8,62 @@
 #include <deque>
 #include <algorithm>
 
-using std::vector;
-using std::string;
 using std::deque;
 using std::ostream;
+using std::string;
+using std::vector;
 
 // Tape head movement direction (Left or Right)
-enum Direction {L, R};
+enum Direction
+{
+    L,
+    R
+};
 
 // Makes my life easier.
 // INPUTS: ostream (usually std::cout) and an array-like object (vector,deque,etc)
 // RETURNS: ostream with formatted contents of the array object
 template <class T>
-void printVector(ostream &os, T v) {
+void printVector(ostream &os, T v)
+{
     os << "{";
-    
-    if (v.empty()) {
+
+    if (v.empty())
+    {
         os << "!! EMPTY SET !! }";
         return;
     }
-    
-    for (int i = 0; i < v.size()-1; ++i) {
+
+    for (int i = 0; i < v.size() - 1; ++i)
+    {
         os << v[i] << ", ";
     }
-    os << v[v.size()-1] << "}";
+    os << v[v.size() - 1] << "}";
 }
 
 // Basic element of the transition function.
 // func(old state, value at tape head) = (new state, write value, direction to move head)
-struct Transition {
+struct Transition
+{
     string currState;
     char currHead;
     string newState;
     char newHead;
     Direction d;
-    
+
     // Basic constructor. Can't be bothered to do deep copies right now.
     Transition(string currState_in, char currHead_in, string newState_in, char newHead_in, Direction d_in)
-    :
-                currState(currState_in),
-                currHead(currHead_in),
-                newState(newState_in),
-                newHead(newHead_in),
-                d(d_in) { }
-    
+        : currState(currState_in),
+          currHead(currHead_in),
+          newState(newState_in),
+          newHead(newHead_in),
+          d(d_in) {}
 };
 
 // Overloaded cout on transition function.
 // (a,b) -> (c,d,L/R)
-ostream& operator<<(ostream& os, const Transition& t) {
+ostream &operator<<(ostream &os, const Transition &t)
+{
     os << "(" << t.currState << ", " << t.currHead << ") -> (" << t.newState << ", " << t.newHead << ", " << t.d << ")";
     return os;
 }
@@ -64,98 +71,112 @@ ostream& operator<<(ostream& os, const Transition& t) {
 // Turing Machine class
 // Member variables are the 7 elements necessary to define a TM.
 // You probably just want to use the TM.run() function.
-class TuringMachine {
+class TuringMachine
+{
 public:
-    
     // Basic constructor with deep copies
     TuringMachine(vector<char> inputAlphabet_in,
                   vector<string> states_in,
                   string initialState_in,
                   vector<string> finalStates_in,
-                  vector<Transition> transitionFunction_in) {
-        
-        for (auto &elt : inputAlphabet_in) {
+                  vector<Transition> transitionFunction_in)
+    {
+
+        for (auto &elt : inputAlphabet_in)
+        {
             inputAlphabet.push_back(elt);
         }
-        
-        for (auto &elt : states_in) {
+
+        for (auto &elt : states_in)
+        {
             states.push_back(elt);
         }
-        
-        for (auto &elt : finalStates_in) {
+
+        for (auto &elt : finalStates_in)
+        {
             finalStates.push_back(elt);
         }
-        
-        for (auto &elt : transitionFunction_in) {
+
+        for (auto &elt : transitionFunction_in)
+        {
             transitionFunction.push_back(elt);
         }
-        
+
         initialState = initialState_in;
-        
     }
-    
+
     // Set initial contents of tape
-    void setInput(string x) {
+    void setInput(string x)
+    {
         // Check for empty string
-        if (x.length() != 0) {
+        if (x.length() != 0)
+        {
             // Set tape value, grow tape if necessary
-            for (int i = 0; i < x.length(); ++i) {
+            for (int i = 0; i < x.length(); ++i)
+            {
                 tape[i] = x[i];
                 tape.push_back('~');
             }
         }
     }
-    
-    bool run() {
-        
+
+    bool run()
+    {
+
         // Set initial state
         currState = initialState;
-        
-        while (true) {
-            
+
+        while (true)
+        {
+
             // Print tape contents for debug purposes
-            //printVector(std::cout, tape);
-            //std::cout << std::endl;
-            
+            // printVector(std::cout, tape);
+            // std::cout << std::endl;
+
             // Accept if currently at an accepting (final) state
-            if (std::find(finalStates.begin(), finalStates.end(), currState) != finalStates.end()) return true;
-            
+            if (std::find(finalStates.begin(), finalStates.end(), currState) != finalStates.end())
+                return true;
+
             bool found = false;
-            
+
             // Try and find a valid transition function for our current state + head combo
-            for (auto &elt : transitionFunction) {
-                if (elt.currState == currState && elt.currHead == tape[headPos]) {
+            for (auto &elt : transitionFunction)
+            {
+                if (elt.currState == currState && elt.currHead == tape[headPos])
+                {
                     // Update state and write to head
                     currState = elt.newState;
                     tape[headPos] = elt.newHead;
-                    
+
                     // Move tape head accordingly
                     (elt.d == L) ? --headPos : ++headPos;
-                    
+
                     // If going past the right end of the tape, grow the tape
-                    if (headPos >= static_cast<int>(tape.size())) {
+                    if (headPos >= static_cast<int>(tape.size()))
+                    {
                         tape.push_back('~');
                     }
-                    
+
                     // If going past the left end of the tape, grow the tape in the negative direction
-                    if (headPos < 0) {
+                    if (headPos < 0)
+                    {
                         tape.push_front('~');
                         // Keep the headPos >= 0 (makes it easier, trust me)
                         // Easier to "fudge" the headPos for high values than try and convert negatives
                         ++headPos;
                     }
-                    
+
                     // we done with this transition
                     found = true;
                     break;
                 }
             }
             // If no valid transition exists, the TM rejects on x
-            if (!found) return false;
+            if (!found)
+                return false;
         }
-        
     }
-    
+
     // 7-tuple parameters
     vector<char> inputAlphabet;
     char blankChar = '~';
@@ -164,49 +185,49 @@ public:
     string initialState;
     vector<string> finalStates;
     vector<Transition> transitionFunction;
-    
+
     // Other various important parameters
     deque<char> tape = {'~'};
     int headPos = 0;
     string currState;
-    
-    
 };
 
 // Overloaded cout on a TM to view its 7-tuple parameters and the current tape
 // Note: usually called at the beginning of the program so tape = input
-ostream& operator<<(ostream& os, const TuringMachine& TM) {
-    
+ostream &operator<<(ostream &os, const TuringMachine &TM)
+{
+
     os << "Input alphabet: ";
     printVector(os, TM.inputAlphabet);
     os << "\n";
-    
+
     os << "Blank character: " << TM.blankChar << "\n";
-    
+
     os << "Tape alphabet: ";
     printVector(os, TM.tapeAlphabet);
     os << "\n";
-    
+
     os << "States: ";
     printVector(os, TM.states);
     os << "\n";
-    
+
     os << "Initial State: " << TM.initialState << "\n";
-    
+
     os << "Final States: ";
     printVector(os, TM.finalStates);
     os << "\n";
-    
+
     os << "Current tape: ";
     printVector(os, TM.tape);
     os << "\n";
-    
+
     os << "Transitions: {\n\t";
-    for (int i = 0; i < TM.transitionFunction.size()-1; ++i) {
+    for (int i = 0; i < TM.transitionFunction.size() - 1; ++i)
+    {
         os << TM.transitionFunction[i] << ",\n\t";
     }
-    os << TM.transitionFunction[TM.transitionFunction.size()-1] << "\n}";
+    os << TM.transitionFunction[TM.transitionFunction.size() - 1] << "\n}";
     os << "\n";
-    
+
     return os;
 }
--- main.cpp
+++ main.cpp
@@ -7,37 +7,131 @@
 using std::cout;
 using std::endl;
 
-int main(int argc, const char * argv[]) {
-    
+int main(int argc, const char *argv[])
+{
+
+    // Binary Increment TM
+    // {
     // Set initial variables according to the 7-tuple definition of a TM
-    vector<char> inputAlphabet = {'0','1'};
-    vector<string> states = {"right","carry","done"};
+    vector<char> inputAlphabet = {'0', '1'};
+    vector<string> states = {"right", "carry", "done"};
     string initialState = "right";
     vector<string> finalStates = {"done"};
-    
+
     // Transitions are defined as: {currState, currHead, newState, writeValue, headDirection}
     vector<Transition> transitionFunction = {
-        Transition("right",'1',"right",'1',R),
-        Transition("right",'0',"right",'0',R),
-        Transition("right",'~',"carry",'~',L),
-        Transition("carry",'1',"carry",'0',L),
-        Transition("carry",'0',"done",'1',L),
-        Transition("carry",'~',"done",'1',L)
-    };
-    
+        Transition("right", '1', "right", '1', R),
+        Transition("right", '0', "right", '0', R),
+        Transition("right", '~', "carry", '~', L),
+        Transition("carry", '1', "carry", '0', L),
+        Transition("carry", '0', "done", '1', L),
+        Transition("carry", '~', "done", '1', L)};
+    // }
+
+    // Divisible by 3 (decimal) TM
+    // {
+    // Set initial variables according to the 7-tuple definition of a TM
+    // vector<char> inputAlphabet = {'0','1','2','3','4','5','6','7','8','9'};
+    // vector<string> states = {"q0","q1","q2", "accept"};
+    // string initialState = "q0";
+    // vector<string> finalStates = {"accept"};
+
+    // // Transitions are defined as: {currState, currHead, newState, writeValue, headDirection}
+    // vector<Transition> transitionFunction = {
+    //     Transition("q0",'0',"q0",'0',R),
+    //     Transition("q0",'3',"q0",'3',R),
+    //     Transition("q0",'6',"q0",'6',R),
+    //     Transition("q0",'9',"q0",'9',R),
+
+    //     Transition("q0",'1',"q1",'1',R),
+    //     Transition("q0",'4',"q1",'4',R),
+    //     Transition("q0",'7',"q1",'7',R),
+
+    //     Transition("q0",'2',"q2",'2',R),
+    //     Transition("q0",'5',"q2",'5',R),
+    //     Transition("q0",'8',"q2",'8',R),
+
+    //     Transition("q0",'~',"accept",'~',R),
+
+    //     Transition("q1",'0',"q1",'0',R),
+    //     Transition("q1",'3',"q1",'3',R),
+    //     Transition("q1",'6',"q1",'6',R),
+    //     Transition("q1",'9',"q1",'9',R),
+
+    //     Transition("q1",'1',"q2",'1',R),
+    //     Transition("q1",'4',"q2",'4',R),
+    //     Transition("q1",'7',"q2",'7',R),
+
+    //     Transition("q1",'2',"q0",'2',R),
+    //     Transition("q1",'5',"q0",'5',R),
+    //     Transition("q1",'8',"q0",'8',R),
+
+    //     Transition("q2",'0',"q2",'0',R),
+    //     Transition("q2",'3',"q2",'3',R),
+    //     Transition("q2",'6',"q2",'6',R),
+    //     Transition("q2",'9',"q2",'9',R),
+
+    //     Transition("q2",'1',"q0",'1',R),
+    //     Transition("q2",'4',"q0",'4',R),
+    //     Transition("q2",'7',"q0",'7',R),
+
+    //     Transition("q2",'2',"q1",'2',R),
+    //     Transition("q2",'5',"q1",'5',R),
+    //     Transition("q2",'8',"q1",'8',R)
+
+    // };
+    // }
+
+    // // Palindrome Checker TM
+    // // {
+    // // Set initial variables according to the 7-tuple definition of a TM
+    // vector<char> inputAlphabet = {'a', 'b'};
+    // vector<string> states = {"start", "haveA", "haveB", "matchA", "matchB", "back", "accept", "reject"};
+    // string initialState = "start";
+    // vector<string> finalStates = {"accept"};
+
+    // // Transitions are defined as: {currState, currHead, newState, writeValue, headDirection}
+    // vector<Transition> transitionFunction = {
+    //     Transition("start", 'a', "haveA", '~', R),
+    //     Transition("start", 'b', "haveB", '~', R),
+    //     Transition("start", '~', "accept", '~', R),
+
+    //     Transition("haveA", 'a', "haveA", 'a', R),
+    //     Transition("haveA", 'b', "haveA", 'b', R),
+    //     Transition("haveA", '~', "matchA", '~', L),
+
+    //     Transition("haveB", 'a', "haveB", 'a', R),
+    //     Transition("haveB", 'b', "haveB", 'b', R),
+    //     Transition("haveB", '~', "matchB", '~', L),
+
+    //     Transition("matchA", 'a', "back", '~', L),
+    //     Transition("matchA", 'b', "reject", 'b', R),
+    //     Transition("matchA", '~', "accept", '~', R),
+
+    //     Transition("matchB", 'a', "reject", 'a', R),
+    //     Transition("matchB", 'b', "back", '~', L),
+    //     Transition("matchB", '~', "accept", '~', R),
+
+    //     Transition("back", 'a', "back", 'a', L),
+    //     Transition("back", 'b', "back", 'b', L),
+    //     Transition("back", '~', "start", '~', R),
+
+    // };
+    // // }
+
     // Create new TM with given input parameters
     TuringMachine TM(inputAlphabet, states, initialState, finalStates, transitionFunction);
-    
+
     // Set initial contents of the tape
-    TM.setInput("1");
-    
+    TM.setInput("101010000000011101001101111111");
+
     // View initial contents and simulate the TM
     cout << TM;
     cout << ((TM.run()) ? "Accepted!" : "Rejected.") << endl;
-    
+
     // Print final tape contents
     printVector(std::cout, TM.tape);
     std::cout << std::endl;
-    
+
     return 0;
 }
